5eeaecf563ce2226decdb8122cd573671ac4bd71
zhan656 <34688799+zhan656@users.noreply.github.com>
2018-06-21 17:35:57 -0700

    app update

--- app.py
+++ app.py
@@ -1,85 +1,142 @@
-# import dependencies 
+# import necessary libraries
+from sqlalchemy import func
 import datetime as dt
 import numpy as np
 import pandas as pd
 
-import sqlalchemy
-from sqlalchemy.ext.automap import automap_base
-from sqlalchemy.orm import Session
-from sqlalchemy import create_engine
+from flask import (
+    Flask,
+    render_template,
+    jsonify,
+    request,
+    redirect)
+from flask import Flask, jsonify, render_template, request, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
+
+import sqlite3
 
 
-# Create engine using the `demographics.sqlite` database file
-engine = create_engine("sqlite:///Jupyter/db/wine_data.sqlite")
-
-# Declare a Base using `automap_base()`
-Base = automap_base()
-
-# Use the Base class to reflect the database tables
-Base.prepare(engine, reflect=True)
-
-# Print all of the classes mapped to the Base
-Base.classes.keys()
-
-# Assign the demographics class to a variable called `Demographics`
-Wine= Base.classes.wine_db
-
-# Create a session
-session = Session(engine)# Create our session (link) from Python to the DB
-
-
-#import necessary libraries
-from flask import (Flask, 
-                   jsonify, 
-                   render_template, 
-                   request, 
-                   redirect)
-
+#################################################
 # Flask Setup
+#################################################
 app = Flask(__name__)
+app.config['JSON_AS_ASCII'] = False  # default
+#################################################
+# Data Setup
+#################################################
 
+# This is how you make a connection
+conn = sqlite3.connect("Wine.DB")
 
+# Write to DB
+wine_data = pd.read_csv("Data/clean_winemag-data.csv")
+wine_data.to_sql("wine", conn, if_exists="replace")
+
+# This will read
+query = "select * from wine"
+wine_df = pd.read_sql(query, conn)
+# # otu_id
+# Otu_id = pd.read_csv("DataSets/Belly_Button_Biodiversity_otu_id.csv")
+# otu_id_df = pd.DataFrame(Otu_id)
+
+# #samples
+# Samples = pd.read_csv("DataSets/Belly_Button_Biodiversity_samples.csv")
+# samples_df = pd.DataFrame(Samples)
+# # Fill NAN values in 'samples' dataframe with 0
+# samples_df = samples_df.fillna(0)
+
+#################################################
 # Flask Routes
+#################################################
 @app.route("/")
-def default():
+# """Return the dashboard homepage."""
+def home():
     return render_template("index.html")
-
-
-@app.route("/columns")
-def columns():
-   # """Return a list of wines."""
-
-    stmnt = session.query(Wine).statement
-    df = pd.read_sql_query(stmnt, session.bind)
-    df.set_index('id', inplace=True)
-
-    # Returning list of the column names (sample names)
-    # return jsonify(list(df.columns))
-    return jsonify(list(df))
-
-@app.route('/wines')
-def list_wines():
-    """Return the MetaData for a given sample."""
     
-    results = session.query(Wine.id, Wine.country, Wine.description, Wine.designation, Wine.points, Wine.price, Wine.province, Wine.region_1, Wine.variety, Wine.winery).all()
 
-    # Create a dictionary entry for each row of metadata information
-    wines = []
-    for result in results:
-        wines.append({
-        "ID":result[0],
-        "Country":result[1],
-        "Description":result[2],
-        "Designation":result[3],
-        "Points":result[4],
-        "Price":result[5],
-        "Province":result[6],
-        "Region":result[7],
-        "Variety":result[8],
-        "Winery":result[9]   
-        })
 
-    return jsonify(wines)
+@app.route('/names')
+# """List of wine variety names.
+# Returns a list of sample names in the format
+# [
+    # "Champagne Blend",
+    # "Champagne Blend",
+    # "Champagne Blend",
+    # "Champagne Blend",
+    # "Champagne Blend",
+    # "Champagne Blend",
+    # "Champagne Blend",
+    # ...
+# ]
 
-if __name__ == '__main__':
+# """
+def names():
+    names = list(set(list(wine_df['variety'])))
+    return jsonify(names)
+
+@app.route('/price_points/<variety>')
+# """List of prices, points and country for a certain wine variety.
+
+# Returns a list of OTU descriptions in the following format
+
+# [
+    # 20,
+    # 40,
+    # 121,
+    # 15,
+    # 65,
+    # ...
+# ]
+# """
+def price_points(variety):
+    prices = list(wine_df[wine_df['variety']==variety]['price'])
+    points = list(wine_df[wine_df['variety']==variety]['points'])
+    countries = list(wine_df[wine_df['variety']==variety]['country'])
+    return jsonify({"points": list(map(int,points)), "prices": list(map(int,prices)), 'countries':countries})
+
+    
+@app.route('/metadata/<variety>')
+# """MetaData for a given sample.
+
+
+# Returns a json dictionary of sample metadata in the format
+
+# {
+    # Rating_Count: 200,
+    # Min_Price: 4,
+    # Max_Price: 2000,
+    # Highest_Rating: "Winery Name"
+    # Lowest_Rating; "Winery Name",
+# }
+# """
+def meta(variety):
+    prices = list(wine_df[wine_df['variety']==variety]['price'])
+    points = list(wine_df[wine_df['variety']==variety]['points'])
+    Rating_Count = len(points)
+    Min_Price = min(prices)
+    Max_Price = max(prices)
+    variety_df = wine_df.loc[wine_df['variety']==variety,['points','winery']]
+    Highest_Rating = list(set(variety_df[variety_df['points']==max(points)]['winery']))
+    Lowest_Rating = list(set(variety_df[variety_df['points']==min(points)]['winery']))
+    # all_meta = wine_df[wine_df['SAMPLEID'] == sampleID]
+    # age = int(all_meta.AGE)
+    # BBTYPE = all_meta.iloc[0]["BBTYPE"]
+    # ETHNICITY = all_meta.iloc[0]["ETHNICITY"]
+    # gender = all_meta.iloc[0]["GENDER"]
+    # location = all_meta.iloc[0]["LOCATION"]
+    sample_metadata = {}
+    sample_metadata['Vareity Name'] = variety 
+    sample_metadata['Rating_Count'] = Rating_Count
+    sample_metadata['Min_Price (USD)'] = Min_Price
+    sample_metadata['Max_Price (USD)'] = Max_Price
+    sample_metadata['Highest Rating Winery'] = Highest_Rating
+    sample_metadata['Lowest Rating Winery'] = Lowest_Rating
+    return jsonify(sample_metadata)
+    
+    
+
+if __name__ == "__main__":
     app.run(debug=True)
+
+
+##############################################################################################################33
